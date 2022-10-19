7f3369d269ebbe206eb02fd31d79bc799707107d
albertito1985 <anulo@live.se>
2022-10-19 11:36:14 +0200

    dropdown arreglado

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -28,23 +28,46 @@ class Button extends Component{
 }
 
 class Dropdown extends Component{
-    constructor(){
+    constructor(props){
         super();
         this.state={
             selected:{
                 value: undefined,
                 phrase:undefined
-            }
+            },
+            showDropdown:false
         }
         this.openClick=this.openClick.bind(this);
         this.handleClick=this.handleClick.bind(this);
     }
 
+    static getDerivedStateFromProps(props,state){
+        if(props.type === "inactive"){
+            console.log(state);
+            state.selected={
+                    value: undefined,
+                    phrase:undefined
+                };
+            state.showDropdown=false
+        }
+    }
+
+    // componentDidMount(){
+    //     if(this.props.type==="inactive"){
+    //         this.setState({
+    //             selected:{
+    //                 value: undefined,
+    //                 phrase:undefined
+    //             },
+    //             showDropdown:false,
+    //         })
+    //     }
+    // }
+
     openClick(){
-        let itemsList= document.getElementById("itemsList");
-        let CustomdropdownContainer = document.getElementById("CustomdropdownContainer");
-        itemsList.classList.toggle("itemListShow");
-        CustomdropdownContainer.classList.toggle("dropdownShow")
+        this.setState({
+            showDropdown:(this.state.showDropdown)?false:true
+        })
     }
 
     generateOptions(optionsArray){
@@ -58,20 +81,16 @@ class Dropdown extends Component{
 
     handleClick(e){
         let target = e.target;
-        let selected = document.getElementById("selectedItem");
-        target.parentNode.children.forEach((div)=>{
-            div.classList.remove("selected");
-        })
-        target.classList.add("selected");
-        selected.textContent =target.textContent;
         this.props.recieveValue(target.attributes.datavalue.value);
-        this.setState({selected:{value: target.attributes.datavalue.value, phrase: target.textContent}});
+        this.setState({
+            selected:{value: String(target.attributes.datavalue.value), phrase: target.textContent},
+        });
     }
 
     generateItems(optionsArray){
         let items = [];
         optionsArray.forEach((optionObject)=>{
-            let div = <div className="item" onClick={this.handleClick} key={optionObject.value} datavalue={optionObject.value}>{optionObject.label}</div>
+            let div = <div className={`item${(this.state.selected.value === String(optionObject.value))?" selected":""}`} onClick={this.handleClick} key={optionObject.value} datavalue={optionObject.value}>{optionObject.label}</div>
             items.push(div);
         });
         return items;
@@ -79,14 +98,16 @@ class Dropdown extends Component{
 
     render(){
         return(
-            <div className={`CustomdropdownContainer${(this.props.type === "inactive")?" CustomdropdownContainerInactive":""}`} 
+            <div className={`CustomdropdownContainer${(this.props.type === "inactive")?" CustomdropdownContainerInactive":""}${(this.state.showDropdown)?" dropdownShow":""}`} 
                 id="CustomdropdownContainer"
                 onClick={(this.props.type!=="inactive")?this.openClick:undefined}
             >
                 <span className={`selectedItem${this.state.selected.phrase?"":" selectedItemEmpty"}`} id="selectedItem">{this.state.selected.phrase || this.props.placeholder}</span>
-                <span className="itemsList" id="itemsList">
-                    {this.generateItems(this.props.options)}
-                </span>
+                {this.state.showDropdown===true && this.props.type !== "inactive" &&
+                    <span className="itemsList" id="itemsList" >
+                        {this.generateItems(this.props.options)}
+                    </span>
+                }
                 <select id="customDropdown" className={`Customdropdown${this.props.type?` ${this.props.type}`:""}`} value={this.state.value}>
                     {this.generateOptions(this.props.options)}
                 </select>
